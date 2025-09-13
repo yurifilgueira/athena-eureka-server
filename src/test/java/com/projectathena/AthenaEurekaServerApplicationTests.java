ff80baea040d646f2f098a0e575619757b759160
yurifilgueira <yurimedeiros141@gmail.com>
2025-09-13 18:35:43 -0300

    feat: Add Eureka Discovery Service

---
+++ src/test/java/com/projectathena/AthenaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectathena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
